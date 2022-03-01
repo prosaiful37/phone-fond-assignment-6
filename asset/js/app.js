d7d3d1f19544e071b39358ec4ee1e954eaecf20f
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-03-01 19:30:27 +0600

    phone details part done

--- asset/js/app.js
+++ asset/js/app.js
@@ -18,11 +18,12 @@ const displaySearchResult = brands => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
-            <img src="${brand.image}" class="card-img-top" alt="...">
+        <div class="card h-100">
+            <img  src="${brand.image}" class="card-img-top w-50 mx-auto py-5 img-fluid" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Brand: ${brand.brand}</h5>
                 <h4 class="card-title">Phone: ${brand.phone_name}</h4>
+                <button onclick="loadPhoneDetails('${brand.slug}')" class="btn btn-warning" type="button">Details</button>
             </div>
         </div> 
         `;
@@ -33,4 +34,43 @@ const displaySearchResult = brands => {
 
     // console.log(brand);
 }
+
+
+const loadPhoneDetails = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => DisplayPhoneDetails(data.data))
+    // console.log(data);
+
+}
+
+const DisplayPhoneDetails = phone =>{
+    console.log(phone.slug);
+    const detailsPhone = document.getElementById('details-phone')
+        const div = document.createElement('div');
+        div.classList.add('col');
+        detailsPhone.innerHTML = `
+        <div class="card ">
+            <img src="${phone.image}" class="card-img-top w-50 mx-auto py-2 img-fluid" alt="...">
+            <hr>
+            <div class="card-body">
+                <h5 class="card-title">${phone.brand}</h5>
+                <h4 class="card-title">${phone.name}</h4>
+                <p class="card-text">ReleaseDate: ${phone.releaseDate}</p>
+                <p class="card-text">ChipSet: ${phone.mainFeatures.chipSet}</p>
+                <p class="card-text">DisplaySize: ${phone.mainFeatures.displaySize}</p>
+                <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
+                <p class="card-text">Storage: ${phone.mainFeatures.storage}</p>
+                
+            </div>
+        </div> 
+        `;
+
+        // detailsPhone.appendChild(div);
+    
+    
+
+}
+
 
